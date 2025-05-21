bfb531d3ce19cc01fa5cfbd51667dee74ad59b98
Achinta27 <achinta27sep1999@gmail.com>
2025-05-21 17:22:53 +0530

    create student model

---
+++ src/models/Students.ts
@@ -0,0 +1,168 @@
+import {
+  CourseFeesType,
+  EmiType,
+  HostelFeeMonthType,
+  HostelFeesType,
+  IStudentType,
+  StudentDataType,
+} from "@/types/StudentType";
+import { generateCustomId } from "@/util/generateCustomId";
+import mongoose, { Schema } from "mongoose";
+
+const emiSchema = new Schema<EmiType>({
+  installmentNumber: {
+    type: Number,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  dueDate: {
+    type: Date,
+  },
+});
+
+const courseFeesSchema = new Schema<CourseFeesType>({
+  totalAmount: {
+    type: Number,
+    required: true,
+  },
+  emis: [emiSchema],
+});
+
+const hostelFeeMonthSchema = new Schema<HostelFeeMonthType>({
+  month: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+});
+
+const hostelFeesSchema = new Schema<HostelFeesType>({
+  monthlyAmount: {
+    type: Number,
+    required: true,
+  },
+  monthsDue: [hostelFeeMonthSchema],
+});
+
+const studentDataSchema = new Schema<StudentDataType>({
+  currentBatch: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Batches",
+    required: true,
+  },
+  currentClass: {
+    type: String,
+  },
+  currentYear: {
+    type: String,
+  },
+
+  bookFees: {
+    type: String,
+  },
+  hostelFees: hostelFeesSchema,
+});
+
+const studentSchema = new Schema<IStudentType>({
+  student_id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  studentName: {
+    type: String,
+    required: true,
+  },
+  mobileNumber: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  dateOfBirth: {
+    type: String,
+    required: true,
+  },
+  gurdianName: {
+    type: String,
+    required: true,
+  },
+  gurdianMobileNumber: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  selectedCourse: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Courses",
+    required: true,
+  },
+  selectedBatch: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Batches",
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  pinCode: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  caste: {
+    type: String,
+    required: true,
+  },
+  class10SchoolName: {
+    type: String,
+  },
+  class10PassYear: {
+    type: String,
+  },
+  class12SchoolName: {
+    type: String,
+  },
+  class12PassYear: {
+    type: String,
+  },
+  courseFees: courseFeesSchema,
+
+  studentData: [studentDataSchema],
+});
+
+studentSchema.pre("save", async function (next) {
+  if (!this.student_id) {
+    try {
+      this.student_id = await generateCustomId(
+        mongoose.model("Students"),
+        "student_id",
+        "STUDENTS-"
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  next();
+});
+
+const Students =
+  mongoose.models.Students || mongoose.model("Students", studentSchema);
+
+export default Students;
