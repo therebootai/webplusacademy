330867cb34789382e2ddd6534a5a769123bbd671
Achinta27 <achinta27sep1999@gmail.com>
2025-05-21 21:32:49 +0530

    modified

---
+++ src/actions/studentAction.ts
@@ -0,0 +1,46 @@
+"use server";
+import { connectToDataBase } from "@/db/connection";
+import Students from "@/models/Students";
+import { generateCustomId } from "@/util/generateCustomId";
+
+export async function createStudent(data: any) {
+  try {
+    await connectToDataBase();
+
+    if (!data.student_id) {
+      data.student_id = await generateCustomId(
+        Students,
+        "student_id",
+        "STUDENTS-"
+      );
+    }
+    const newStudent = new Students(data);
+    await newStudent.save();
+
+    return { success: true, student: newStudent };
+  } catch (error: any) {
+    console.error("Error creating student:", error);
+    return { success: false, error: error.message || "Unknown error" };
+  }
+}
+
+export async function getStudents(filter = {}) {
+  try {
+    await connectToDataBase();
+
+    const students = await Students.find(filter)
+      .populate({
+        path: "studentData.currentBatch",
+        model: "Batches",
+      })
+      .populate({
+        path: "studentData.currentCourse",
+        model: "Courses",
+      });
+
+    return { success: true, students };
+  } catch (error: any) {
+    console.error("Error fetching students:", error);
+    return { success: false, error: error.message || "Unknown error" };
+  }
+}
