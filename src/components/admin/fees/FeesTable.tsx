f5e277d23aa4953dd0444e5aec3eccf40f6457a9
fireFistAniket <sahaaniket720@gmail.com>
2025-05-22 17:52:06 +0530

    modified

--- src/components/admin/fees/FeesTable.tsx
+++ src/components/admin/fees/FeesTable.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { IStudentType } from "@/types/StudentType";
+import DisplayTable from "@/ui/DisplayTable";
+import { FaCheckCircle, FaClock } from "react-icons/fa";
 
 export default function FeesTable({
   studentsData,
@@ -12,8 +14,75 @@ export default function FeesTable({
     "Mobile Number",
     "Hostel fees",
     "Course Fees",
+    ...Array.from({ length: 4 }, (_, index) => `EMI ${index + 1}`),
     "total due",
   ];
 
-  return <div>FeesTable</div>;
+  return (
+    <DisplayTable tableHeader={tableHeader}>
+      {studentsData.map((student) => {
+        const studentInfo = student.studentData[0];
+
+        const course = studentInfo?.currentCourse;
+        const batch = studentInfo?.currentBatch;
+
+        const isCoursePopulated =
+          course && typeof course === "object" && "_id" in course;
+        const isBatchPopulated =
+          batch && typeof batch === "object" && "_id" in batch;
+
+        return (
+          <div
+            key={student._id as string}
+            className="flex odd:bg-white even:bg-site-darkgreen/5 p-2.5"
+          >
+            <div
+              className="flex-1"
+              style={{ flexBasis: `${Math.round(100 / tableHeader.length)}%` }}
+            >
+              {student.studentName}
+            </div>
+            <div
+              className="flex-1"
+              style={{ flexBasis: `${Math.round(100 / tableHeader.length)}%` }}
+            >
+              {student.mobileNumber}
+            </div>
+            <div
+              className="flex-1"
+              style={{ flexBasis: `${Math.round(100 / tableHeader.length)}%` }}
+            >
+              {student.courseFees.totalAmount}
+            </div>
+            {Array.from({ length: 4 }).map((_, index) => {
+              const emi = student.courseFees.emis[index];
+
+              return (
+                <div
+                  key={index}
+                  className="flex-1 inline-flex items-center justify-center gap-2"
+                  style={{
+                    flexBasis: `${Math.round(100 / tableHeader.length)}%`,
+                  }}
+                >
+                  {emi ? (
+                    <>
+                      {emi.paid ? (
+                        <FaCheckCircle className="text-green-600 text-base" />
+                      ) : (
+                        <FaClock className="text-red-600 text-base" />
+                      )}
+                      {emi.amount}
+                    </>
+                  ) : (
+                    <>-</>
+                  )}
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </DisplayTable>
+  );
 }
