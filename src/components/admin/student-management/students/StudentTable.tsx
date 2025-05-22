ff23d57999889bcb9db745cb515b1fb75ff1a3e5
Achinta27 <achinta27sep1999@gmail.com>
2025-05-22 17:48:22 +0530

    modified

--- src/components/admin/student-management/students/StudentTable.tsx
+++ src/components/admin/student-management/students/StudentTable.tsx
@@ -2,8 +2,10 @@
 import { deleteStudent } from "@/actions/studentAction";
 import { IStudentType } from "@/types/StudentType";
 import DisplayTable from "@/ui/DisplayTable";
+import SidePopUpSlider from "@/ui/SidePopup";
 import { useRouter } from "next/navigation";
-import { useTransition } from "react";
+import { useState, useTransition } from "react";
+import AddNewStudent from "./AddNewStudent";
 
 export default function StudentTable({
   studentsData,
@@ -21,6 +23,10 @@ export default function StudentTable({
   ];
   const [isPending, startTransition] = useTransition();
   const router = useRouter();
+  const [showPopUp, setShowPopUp] = useState(false);
+  const [selectedStudent, setSelectedStudent] = useState<IStudentType | null>(
+    null
+  );
 
   const handleDelete = (studentId: string) => {
     const confirmed = confirm("Are you sure you want to delete this student?");
@@ -37,41 +43,64 @@ export default function StudentTable({
   };
 
   return (
-    <DisplayTable tableHeader={tableHeader}>
-      {studentsData.map((student) => (
-        <div
-          key={student._id as string}
-          className="flex odd:bg-white even:bg-site-darkgreen/5 p-2.5"
-          style={{ flexBasis: `${Math.round(100 / studentsData.length)}%` }}
+    <>
+      <DisplayTable tableHeader={tableHeader}>
+        {studentsData.map((student) => (
+          <div
+            key={student._id as string}
+            className="flex odd:bg-white even:bg-site-darkgreen/5 p-2.5"
+            style={{ flexBasis: `${Math.round(100 / studentsData.length)}%` }}
+          >
+            <div className="flex-1">{student.studentName}</div>
+            <div className="flex-1">{student.mobileNumber}</div>
+            <div className="flex-1">
+              {student.studentData[0]?.currentCourse?.course_name}
+            </div>
+            <div className="flex-1">
+              {student.studentData[0]?.currentBatch?.batch_name}
+            </div>
+            <div className="flex-1">
+              {student.address} , {student.city} , {student.pinCode}
+            </div>
+            <div className="flex-1">{student.gurdianMobileNumber}</div>
+            <div className="flex-1 flex gap-1 items-center">
+              <button
+                onClick={() => {
+                  setSelectedStudent(student);
+                  setShowPopUp(true);
+                }}
+                type="button"
+                className="text-shadow-site-black"
+              >
+                Edit
+              </button>
+              |
+              <button
+                type="button"
+                className="text-red-500"
+                onClick={() => handleDelete(student.student_id as string)}
+                disabled={isPending}
+              >
+                {isPending ? "Deleting..." : "Delete"}
+              </button>
+            </div>
+          </div>
+        ))}
+      </DisplayTable>
+      {selectedStudent && (
+        <SidePopUpSlider
+          showPopUp={showPopUp}
+          handleClose={() => setShowPopUp(false)}
         >
-          <div className="flex-1">{student.studentName}</div>
-          <div className="flex-1">{student.mobileNumber}</div>
-          <div className="flex-1">
-            {student.studentData[0]?.currentCourse?.course_name}
-          </div>
-          <div className="flex-1">
-            {student.studentData[0]?.currentBatch?.batch_name}
-          </div>
-          <div className="flex-1">
-            {student.address} , {student.city} , {student.pinCode}
-          </div>
-          <div className="flex-1">{student.gurdianMobileNumber}</div>
-          <div className="flex-1 flex gap-1 items-center">
-            <button type="button" className="text-shadow-site-black">
-              Edit
-            </button>
-            |
-            <button
-              type="button"
-              className="text-red-500"
-              onClick={() => handleDelete(student.student_id as string)}
-              disabled={isPending}
-            >
-              {isPending ? "Deleting..." : "Delete"}
-            </button>
-          </div>
-        </div>
-      ))}
-    </DisplayTable>
+          <AddNewStudent
+            existingStudent={selectedStudent}
+            onSuccess={() => {
+              router.refresh();
+              setShowPopUp(false);
+            }}
+          />
+        </SidePopUpSlider>
+      )}
+    </>
   );
 }
