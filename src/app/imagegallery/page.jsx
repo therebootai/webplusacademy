c9a0e6feec35f2be0d4227e7c5d2000f23cc69fb
fireFistAniket <sahaaniket720@gmail.com>
2025-10-24 22:38:08 +0530

    upload

--- src/app/imagegallery/page.jsx
+++ src/app/imagegallery/page.jsx
@@ -1,32 +1,54 @@
-import Gallery from '@/components/gallery/Gallery'
-import Subbanner from '@/components/global/Subbanner'
-import MainTemplates from '@/templates/Maintemplates'
-import React from 'react'
-
-
+import { getAllGalleries } from "@/actions/galleryActions";
+import Gallery from "@/components/gallery/Gallery";
+import GalleryList from "@/components/gallery/GalleryList";
+import Subbanner from "@/components/global/Subbanner";
+import MainTemplates from "@/templates/Maintemplates";
+import React from "react";
 
 export const metadata = {
   title: "Gallery | Waveplus Academy – NEET Coaching in Siliguri",
   description:
     "View our gallery showcasing classroom sessions, student achievements, and events at Waveplus Academy – the leading NEET coaching center in Siliguri.",
-}
+};
 
-
-const page = () => {
+const page = async () => {
+  const data = await getPageData();
   return (
     <>
-    
-     <MainTemplates>
-      <Subbanner heading="Image Gallery"/>
-      <section className="px-4 md:px-40">
-
-      <Gallery/>
-      </section>
-
-     </MainTemplates>
-    
+      <MainTemplates>
+        <Subbanner heading="Image Gallery" />
+        <section className="px-4 md:px-40 py-10">
+          {data && <GalleryList data={data?.data} type={"image"} />}
+        </section>
+      </MainTemplates>
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;
+
+async function getPageData() {
+  try {
+    const data = await getAllGalleries(
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      "image"
+    );
+    return data;
+  } catch (error) {
+    console.log(error);
+    return {
+      success: false,
+      data: [],
+      pagination: {
+        totalCount: 0,
+        currentPage: 1,
+        limit: 10,
+        totalPages: 0,
+      },
+    };
+  }
+}
